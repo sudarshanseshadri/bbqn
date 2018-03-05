f4095bb28ead0866374d03bae49ba5c37c5e8fb2
kristychoi <kristy.choi24@gmail.com>
2018-03-04 17:42:45 -0800

    typo

--- new_learn.py
+++ new_learn.py
@@ -21,7 +21,7 @@ import logging
 OptimizerSpec = namedtuple("OptimizerSpec", ["constructor", "kwargs", "lr_schedule"])
 
 # use GPU if available
-USA_CUDA = True
+USE_CUDA = True
 # USE_CUDA = torch.cuda.is_available()
 FloatTensor = torch.cuda.FloatTensor if USE_CUDA else torch.FloatTensor
 LongTensor = torch.cuda.LongTensor if USE_CUDA else torch.LongTensor
